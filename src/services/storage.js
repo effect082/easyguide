13e3ea1e7e0ae34fffa63caf7bfe201aec9f01da
effect082 <effect1004@gmail.com>
2025-12-13 00:01:00 +0900

    fix: switch to google sheets and cleanup adapters

--- src/services/storage.js
+++ src/services/storage.js
@@ -1,16 +1,9 @@
 import { localStorageAdapter } from './localStorageAdapter.js';
-import { supabaseAdapter } from './supabaseAdapter.js';
-import { airtableAdapter } from './airtableAdapter.js';
 import { googleSheetsAdapter } from './googleSheetsAdapter.js';
 
-export const isSupabaseEnabled = false;
-export const isAirtableEnabled = false;
 export const isGoogleSheetsEnabled = true;
 
-// Storage priority: Google Sheets > Airtable > Supabase > LocalStorage
+// Storage priority: Google Sheets > LocalStorage
 export const storage = isGoogleSheetsEnabled
     ? googleSheetsAdapter
-    : (isAirtableEnabled
-        ? airtableAdapter
-        : (isSupabaseEnabled ? supabaseAdapter : localStorageAdapter));
-
+    : localStorageAdapter;
